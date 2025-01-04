6cb795c30b9f33b55da8df24d5cd517c4cbbe26c
MG-Developers <mohitjain24052002@gmail.com>
2025-01-04 17:01:03 +0530

    user authentication completed

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
-
 const cors = require("cors");
-
 const app = express();
+const cookieParser = require("cookie-parser");
+const connectToDB = require("./db/db");
+
+const userRoute = require("./routes/user.route");
+
+connectToDB();
 
 app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRoute);
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!");
